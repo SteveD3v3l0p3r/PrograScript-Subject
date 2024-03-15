4f9a1b89bc6b30448d26be87427398f18a076996
Gerson Ramirez <128430318+SteveD3v3l0p3r@users.noreply.github.com>
2024-03-15 02:07:42 +0000

    calculadora con opcion de exponencia

--- intro.sh
+++ intro.sh
@@ -9,6 +9,7 @@ echo "1. SUMA"
 echo "2. RESTA"
 echo "3. MULTIPLICACION"
 echo "4. DIVISION"
+echo "5. EXPONENCIACION"
 
 read -p "Seleccione una opcion: " opcion
 
@@ -20,6 +21,8 @@ elif [ $opcion -eq 3 ]; then
     respuesta=$((num1 * num2))
 elif [ $opcion -eq 4 ]; then
     respuesta=$((num1 / num2))
+elif [ $opcion -eq 5 ]; then
+    respuesta=$((num1 ** num2))
 fi
 
 echo "La respuesta es: $respuesta" 
