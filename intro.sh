1bf32976dd26d63702284f4713f45922d0285574
Gerson Ramirez <128430318+SteveD3v3l0p3r@users.noreply.github.com>
2024-03-15 01:53:34 +0000

    calculadora

--- intro.sh
+++ intro.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-nombre="Gerson Ramirez"
-edad=15
+echo "***CALCULADORA BASICA***"
 
-if [ $edad -ge 18 ]; then
-    echo "$nombre, eres mayor de edad."
-else
-    echo "$nombre, no eres mayor de edad."
+read -p "Ingrese el primer numero: " num1
+read -p "Ingrese el segundo numero: " num2
+
+echo "1. SUMA"
+echo "2. RESTA"
+echo "3. MULTIPLICACION"
+echo "4. DIVISION"
+
+read -p "Seleccione una opcion: " opcion
+
+if [ $opcion -eq 1 ]; then
+    respuesta=$((num1 + num2))
+elif [ $opcion -eq 2 ]; then
+    respuesta=$((num1 - num2))
+elif [ $opcion -eq 3 ]; then
+    respuesta=$((num1 * num2))
+elif [ $opcion -eq 4 ]; then
+    respuesta=$((num1 / num2))
 fi
+
+echo "La respuesta es: $respuesta" 
