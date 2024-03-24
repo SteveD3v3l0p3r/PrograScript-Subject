f61115d1bd09933bbde69e2d896089af51356763
Gerson Ramirez <128430318+SteveD3v3l0p3r@users.noreply.github.com>
2024-03-24 04:34:06 +0000

    buscador de archivos

--- intro.sh
+++ intro.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-archivo="prueba.txt"
+read -p "¿Que archivo deseas buscar? (añade su extension): " archivo
 
-cat $archivo
+if [ -e "$archivo" ]; then
+    echo "Ejecutando archivo: $archivo...."
+    echo "********************************"
+    cat $archivo
+else
+    read -p "El archivo '$archivo' no existe, ¿deseas crearlo? (S/N)" resp
+    if [ $resp = "s" ]; then
+        touch $archivo
+        nano $archivo
+    else 
+        echo "Saliendo del buscador..."
+    fi
+fi
