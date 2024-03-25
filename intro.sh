2a0fc39cad636bd0ebfeb29d995ab4aac5700ce2
Gerson Ramirez <128430318+SteveD3v3l0p3r@users.noreply.github.com>
2024-03-25 00:34:45 +0000

    conversor

--- intro.sh
+++ intro.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-read -p "¿Que archivo deseas buscar? (añade su extension): " archivo
+#read -p "¿Que archivo deseas buscar? (añade su extension): " archivo
 
-if [ -e "$archivo" ]; then
-    echo "Ejecutando archivo: $archivo...."
-    echo "********************************"
-    cat $archivo
-else
-    read -p "El archivo '$archivo' no existe, ¿deseas crearlo? (S/N)" resp
-    if [ $resp = "s" ]; then
-        touch $archivo
-        nano $archivo
-    else 
-        echo "Saliendo del buscador..."
-    fi
-fi
+#if [ -e "$archivo" ]; then
+ #   echo "Ejecutando archivo: $archivo...."
+  #  echo "********************************"
+  #  cat $archivo
+#else
+ #   read -p "El archivo '$archivo' no existe, ¿deseas crearlo? (S/N)" resp
+  #  if [ $resp = "s" ]; then
+   #     touch $archivo
+    #    nano $archivo
+    #else 
+     #   echo "Saliendo del buscador..."
+    #fi
+#fi
+
+
+function redondear(){
+    echo "El resultado redondeado a 2 decimales es: $(echo "scale=2; $1" | bc | xargs printf %0.2f)"
+}
+
